a278bf0a145534fd09044e0ddde451f847040526
knkayser <knkayser@gmail.com>
2018-10-10 10:47:09 -0500

    final edits

--- config.py
+++ config.py
@@ -1,2 +1,1 @@
-client_id = "1YFcljdSAVoMT7GQBhDtkw"
-api_key = "e7imh2An41nkt9Uj0FH6SEk6letRxECo0k5IUeDRUykbfMWnX5r3pGGvJCbAhJwJmQlCbF8FkDTkaHRMDZidsq8ME-dnns7jfM1UTfVZgzBwD4oI4fVEV22L_72yW3Yx"
+gkey = "AIzaSyDw9aM0MJTZeycFriehXOBV7sHGmhU9afo"
